0b757e4430e05ee16af60e46a765d949739c885f
John R. Storey <john@binaryfeline.com>
2017-11-10 23:56:33 -0500

    Inital implementation of view.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PlanetView from './components/PlanetView';
+
+class App extends Component {
+  render() {
+    return (
+      <PlanetView />
+    );
+  }
+}
+
+export default App;
