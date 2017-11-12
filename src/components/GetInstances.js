d6469246f562775120e2ca7e498d588945d71763
John R. Storey <john@binaryfeline.com>
2017-11-11 22:13:13 -0800

    Added comments.
    - also removed debug 'console.log' statements.

--- src/components/GetInstances.js
+++ src/components/GetInstances.js
@@ -1,13 +1,16 @@
+/*
+ * Sample of creating a view per component
+ * state in a nice, testable way.
+ */
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchInstanceAction } from "../actions/fetchInstanceAction";
 import { sleep } from "../libs/helpers";
 
-// A View per state.
+// Make a view per state.
 const LoadingView = () => <div>Loading ...</div>;
 const ErrorView = () => <div>Error</div>;
 const InstancesView = instances => {
-  console.log("instances view instances", instances);
   return (
     <ul>
       {instances.instances.map(instance => {
@@ -17,24 +20,20 @@ const InstancesView = instances => {
   );
 };
 
+// Determine which view to return based on the state.
 const DisplayByState = ({ loading, instances }) => {
-  console.log("loading", loading);
-  console.log("instances", instances);
   if (loading) {
     return <LoadingView />;
   } else if (instances) {
-    console.log("DisplayByState instances", instances);
     return <InstancesView {...{ instances }} />;
   } else {
     return <ErrorView />;
   }
 };
 
-// State:
-// { Loading: true }
-// { Loading: false, instances: [ { instanceId: '1' }, ... ]}
-// { Loading: false, error: any }
-
+// Main container code. Works with redux to change
+// redux state, and changes the view shown based off
+// that state.
 class GetInstancesContainer extends Component {
   async componentDidMount() {
     await sleep(3000);
@@ -42,12 +41,11 @@ class GetInstancesContainer extends Component {
   }
 
   render() {
-    console.log("GetInstances render state", this.state);
-    console.log("GetInstances render props", this.props);
     return <DisplayByState {...this.props} namespace={this.props.namespace} />;
   }
 }
 
+// Finally standard redux patterns.
 const mapStateToProps = (state, ownProps) => {
   console.log("mapStateToProps state", state);
   return {
